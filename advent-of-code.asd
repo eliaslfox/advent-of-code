f84b5b24a1270e7a76103db5b5b915b4353b8bba
Elias Lawson-Fox <me@eliaslfox.com>
2021-11-30 18:36:22 -0800

    Init

---
+++ advent-of-code.asd
@@ -0,0 +1,5 @@
+(asdf:defsystem #:advent-of-code
+  :depends-on (#:coalton)
+  :pathname "src/"
+  :serial t
+  :components ((:file "day-1")))
---
+++ src/day-1.lisp
@@ -0,0 +1,7 @@
+(uiop:define-package #:day-1
+  (:use #:coalton #:coalton-library))
+
+(in-package #:coalton-library)
+
+(coalton-toplevel
+  (define-type T))
